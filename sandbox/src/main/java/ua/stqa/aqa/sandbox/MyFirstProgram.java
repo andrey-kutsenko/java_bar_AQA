47db893adcd4a967ed0427869f9e57c8102cee68
Andrey <andrei.kutsenko@gmail.com>
2021-02-05 15:01:14 +0200

    added Square and Rect

--- sandbox/src/main/java/ua/stqa/aqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/aqa/sandbox/MyFirstProgram.java
@@ -2,7 +2,23 @@ package ua.stqa.aqa.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, World!!!");
+    hello("Andrey");
+    hello("World");
+
+
+    Square s=new Square(9);
+    Rectangle r=new Rectangle(5,8);
+
+
+
+
+    System.out.println(r.area());
+    System.out.println(s.area());
   }
+  public static void hello(String somebody){
+    System.out.println("Hello, "+somebody+"!");
+  }
+
+
 
 }
