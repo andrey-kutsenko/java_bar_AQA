ce7638effc906ca0f53c4757e66d13ac2abe7775
Andrey <andrei.kutsenko@gmail.com>
2021-02-06 22:04:35 +0200

    Address-web-tests project is added

--- sandbox/src/test/java/ua/stqa/aqa/sandbox/SquareTest.java
+++ sandbox/src/test/java/ua/stqa/aqa/sandbox/SquareTest.java
@@ -1,5 +1,6 @@
 package ua.stqa.aqa.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -7,7 +8,9 @@ public class SquareTest {
   @Test
   public void testArea(){
     Square s=new Square(5);
-    assert s.area()==25;
+    Assert.assertEquals(s.area(),25);
+    System.out.println("!");
+
 
 
   }
