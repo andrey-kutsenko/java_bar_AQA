002b765aaf878b18045ec4a2c1b4cc3908f2a808
Andrey <andrei.kutsenko@gmail.com>
2021-02-09 18:10:55 +0200

    Choosing of browser is added

--- addressbook-web-tests/src/test/java/ua/stqa/aqa/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ua/stqa/aqa/addressbook/appmanager/ApplicationManager.java
@@ -2,16 +2,32 @@ package ua.stqa.aqa.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 public class ApplicationManager {
   WebDriver driver;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
 
   public void init() {
-    driver = new ChromeDriver();
+    if (browser==BrowserType.CHROME){
+      driver=new ChromeDriver();
+    }
+    else if(browser==BrowserType.FIREFOX){
+      driver=new FirefoxDriver();
+    }
+    else if(browser==BrowserType.IE){
+      driver=new InternetExplorerDriver();
+    }
     driver.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(driver);
     navigationHelper = new NavigationHelper(driver);
