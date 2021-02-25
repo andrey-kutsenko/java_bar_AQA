4a63c5e76827cd17ddf3faafbaa2a5f6dfb3aea9
Andrey <andrei.kutsenko@gmail.com>
2021-02-25 16:37:53 +0200

    user adding test created

--- mantis-tests/src/test/java/ua/stqa/aqa/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ua/stqa/aqa/mantis/appmanager/ApplicationManager.java
@@ -18,6 +18,7 @@ public class ApplicationManager {
   private String browser;
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
+  private MailHelper mailHelper;
 
   public ApplicationManager(String browser){
     this.browser = browser;
@@ -51,7 +52,12 @@ public class ApplicationManager {
     }
     return ftp;
   }
-
+  public MailHelper mail(){
+    if(mailHelper==null) {
+      mailHelper = new MailHelper(this);
+    }
+    return mailHelper;
+  }
   public WebDriver getDriver() {
     if(driver==null){
       if (browser.equals(BrowserType.CHROME)){
--- mantis-tests/src/test/java/ua/stqa/aqa/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ua/stqa/aqa/mantis/appmanager/RegistrationHelper.java
@@ -1,18 +1,25 @@
 package ua.stqa.aqa.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
-public class RegistrationHelper {
-  private final ApplicationManager app;
-  private WebDriver driver;
+public class RegistrationHelper extends HelperBase {
 
   public RegistrationHelper(ApplicationManager app) {
-    this.app=app;
-    driver=app.getDriver();
+    super(app);
   }
 
   public void start(String username, String email) {
     driver.get(app.getProperty("web.BaseURL")+"/signup_page.php");
    // driver.get("http://localhost/mantisbt-1.3.20/signup_page.php");
+    type(By.name("username"),username);
+    type(By.name("email"),email);
+    click(By.cssSelector("input[value='Signup']"));
+   // <input type="submit" class="button" value="Зарегистрироваться">  //input[@class='button']
+  }
+  public void finish(String confirmationLink, String password) {
+    driver.get(confirmationLink);
+    type(By.name("password"),password);
+    type(By.name("password_confirm"),password);
+    click(By.cssSelector("input[value='Update User']"));
   }
 }
