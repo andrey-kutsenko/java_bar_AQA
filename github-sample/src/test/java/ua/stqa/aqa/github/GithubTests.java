fcd66b0bbf511e309714fa099fd4d5abdfe4c3d3
Andrey <andrei.kutsenko@gmail.com>
2021-03-03 14:37:36 +0200

    REST test improving

--- github-sample/src/test/java/ua/stqa/aqa/github/GithubTests.java
+++ github-sample/src/test/java/ua/stqa/aqa/github/GithubTests.java
@@ -12,7 +12,7 @@ public class GithubTests {
   @Test
   public void testCommits() throws IOException {
     Github github = new RtGithub("8edbb6fcaa751d8cc3b99649ba472b7633c76632");
-    RepoCommits commits = github.repos().get(              j;j;j;jnew Coordinates.Simple("andrey-kutsenko", "java_bar_AQA"))
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("andrey-kutsenko", "java_bar_AQA"))
             .commits();
     for(RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String,String>().build())){
       System.out.println(new RepoCommit.Smart(commit).message());
